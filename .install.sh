96045d8d01fdde5a71d6ec16245d9468fefcf835
Thomas Miller <thomas.miller@pgn.com>
2017-10-13 13:25:29 -0700

    Fixed echo syntax error and added color to my echo statements

--- .install.sh
+++ .install.sh
@@ -3,83 +3,88 @@
 # Script to install prereqs for dotfiles to work correctly.
 # This ensures TMUX, ZSH, and Python is installed systemwide
 # Also run's ~/.fonts/install.sh, vundle, and ~/.vim/bundle/YouCompleteMe/install.sh
+
+# Color variables
+RED='\033[0;31m'
+NC='\033[0m' # No Color
+
 DIST=`cat /etc/*-release | grep ^ID= | tr -d ID=\"\"`
 case $DIST in
   "ubuntu")
-    echo Distro is Ubuntu, using APT
+    echo -e "${RED}Distro is Ubuntu, using APT${NC}"
 
     # if Python
     if [ "`/usr/bin/dpkg-query --show --showformat='${db:Status-Status}\n' 'python-dev'`" == "not-installed" ]; then
-      echo Installing python-dev
+      echo -e "${RED}Installing python-dev${NC}"
       sudo apt-get install python-dev
     else
-      echo python is installed
+      echo -e "${RED}python is installed${NC}"
     fi
 
     # if Zsh
     if [ "`/usr/bin/dpkg-query --show --showformat='${db:Status-Status}\n' 'zsh'`" == "not-installed" ]; then
-      echo Installing zsh
+      echo -e "${RED}Installing zsh${NC}"
       sudo apt-get install zsh
     else
-      echo zsh is installed
+      echo -e "${RED}zsh is installed${NC}"
     fi
 
     # if Tmux
     if [ "`/usr/bin/dpkg-query --show --showformat='${db:Status-Status}\n' 'tmux'`" == "not-installed" ]; then
-      echo Installings tmux
+      echo -e "${RED}Installings tmux${NC}"
       sudo apt-get install tmux
     else
-      echo tmux is installed
+      echo -e "${RED}tmux is installed${NC}"
     fi
     ;;
 
   "rhel")
-    echo Distro is RHEL, using yum
+    echo -e "${RED}Distro is RHEL, using yum${NC}"
 
     # if Python
     if [ `rpm -q python-dev 1&>/dev/null` ]; then
-      echo Installing python-devel
+      echo -e "${RED}Installing python-devel${NC}"
       sudo yum install python-devel
     else
-      echo python is installed
+      echo -e "${RED}python is installed${NC}"
     fi
 
     # if Zsh
     if [ `rpm -q zsh 1&>/dev/null` ]; then
-      echo Installing zsh
+      echo -e "${RED}Installing zsh${NC}"
       sudo yum install zsh
     else
-      echo zsh is installed
+      echo -e "${RED}zsh is installed${NC}"
     fi
 
     # if Tmux
     if [ `rpm -q tmux 1&>/dev/null` ]; then
-      echo Installing tmux
+      echo -e "${RED}Installing tmux${NC}"
       sudo yum install tmux
     else
-      echo tmux is installed
+      echo -e "${RED}tmux is installed${NC}"
     fi
     ;;
 
   *)
-    echo "Dotfiles installation is either unable to detect distro or distro is not supported"
+    echo -e "${RED}Dotfiles installation is either unable to detect distro or distro is not supported${NC}"
     exit 1
 esac
 
 # Moving everything in this dir to ~/
 if [ ! "${HOME}" == ${PWD} ]; then
-  echo Installing dotfiles into homedir
+  echo -e "${RED}Installing dotfiles into homedir${NC}"
   shopt -s dotglob
   mv * ~/.
   shopt -u dotglob
 fi
 
-echo Running font installation script
+echo -e "${RED}Running font installation script${NC}"
 ~/.fonts/install.sh
 
-echo Running vundle install (silently)
-vim +PluginInstall +qall &>/dev/null
+echo -e "${RED}Running vundle install (silently)${NC}"
+vim +PluginInstall +qall #&>/dev/null
 
-echo Compiling YouCompleteMe ycmd
-~/.vim/bundle/YouComleteMe/install.sh
+echo -e "${RED}Compiling YouCompleteMe ycmd${NC}"
+sh ~/.vim/bundle/YouCompleteMe/install.sh
 
